90221a960000db445923dad1b8ece771872483c4
Fenny <25108519+Fenny@users.noreply.github.com>
2020-03-11 16:54:08 +0100

    v0.0.1

--- main.go
+++ main.go
@@ -12,9 +12,9 @@ import (
 
 // Config ...
 type Config struct {
-	// Skip defines a function to skip middleware.
+	// Filter defines a function to skip middleware.
 	// Optional. Default: nil
-	Skip func(*fiber.Ctx) bool
+	Filter func(*fiber.Ctx) bool
 	// XSSProtection
 	// Optional. Default value "1; mode=block".
 	XSSProtection string
@@ -64,8 +64,8 @@ func New(config ...Config) func(*fiber.Ctx) {
 	}
 	// Return middleware handler
 	return func(c *fiber.Ctx) {
-		// Skip middleware if Skip returns true
-		if cfg.Skip != nil && cfg.Skip(c) {
+		// Filter request to skip middleware
+		if cfg.Filter != nil && cfg.Filter(c) {
 			c.Next()
 			return
 		}
